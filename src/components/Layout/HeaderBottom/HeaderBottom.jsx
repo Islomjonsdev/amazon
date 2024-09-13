ed2ea5abbc689e9b4a841127fe4edfb9a9b15d5a
Islomjonsdev <islomjon@immigrateable.com>
2024-09-13 17:53:01 +0500

    added sidebar function

--- src/components/Layout/HeaderBottom/HeaderBottom.jsx
+++ src/components/Layout/HeaderBottom/HeaderBottom.jsx
@@ -4,24 +4,34 @@ import { Link } from "react-router-dom";
 import data from "../../../api/dummiy-data.json";
 import { v4 as uuidv4 } from "uuid";
 import style from "./HeaderBottom.module.scss";
+import { useState } from "react";
 
 const HeaderBottom = () => {
+  const [openSidebarPopup, setOpenSidebarPopup] = useState(false);
+  const jandleCloneAndOpenPopup = () => {
+    setOpenSidebarPopup((openandClose) => !openandClose);
+  };
   return (
-    <div className={style.header__bottom}>
-      <div className={style.header__bottom__btn__blog}>
-        <button onClick={() => setModal(true)}>
-          <GiHamburgerMenu />
-          All
-        </button>
-        <ul>
-          {data?.map((item) => (
-            <li key={uuidv4()}>
-              <Link>{item?.link}</Link>
-            </li>
-          ))}
-        </ul>
+    <>
+      <div className={style.header__bottom}>
+        <div className={style.header__bottom__btn__blog}>
+          <button onClick={jandleCloneAndOpenPopup}>
+            <GiHamburgerMenu />
+            All
+          </button>
+          <ul>
+            {data?.map((item) => (
+              <li key={uuidv4()}>
+                <Link>{item?.link}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-    </div>
+      {openSidebarPopup && (
+        <div>salom</div>
+      )}
+    </>
   );
 };
 
