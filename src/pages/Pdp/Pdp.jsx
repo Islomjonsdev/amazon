229397c163e1554d7164bd14782f7df2e2d1fa85
Islomjonsdev <islomjon@immigrateable.com>
2024-10-23 22:29:48 +0500

    pdp added loading

--- src/pages/Pdp/Pdp.jsx
+++ src/pages/Pdp/Pdp.jsx
@@ -1,22 +1,31 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import Loading from "../../components/Loading/Loading";
 import style from "./Pdp.module.scss";
 
 const Pdp = () => {
   const { id } = useParams();
+  const [ loading, setLoading ] = useState(false)
   const [exploreNowId, setExploreNowId] = useState(null);
 
   useEffect(() => {
+    setLoading(true)
     axios(`https://658efbfd2871a9866e7a1bb4.mockapi.io/work/${id}`)
       .then((res) => {
         setExploreNowId(res.data)
         console.log(res?.data);
+        setLoading(false)
       })
       .catch((err) => {
         console.log(err);
+        setLoading(true)
       })
   }, []);
+
+  if (loading) {
+    return <Loading />
+  }
 
   return (
     <div className={style.pdp}>
